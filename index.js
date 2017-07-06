b0ade8022e14024592ff10221db73bdaca934150
Cameron Cabo <ccabo@seas.upenn.edu>
2017-07-06 11:45:34 -0700

    added mongo and mongoose

---
+++ index.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connecction successful');
+
+  kittySchema.methods.speak = () => {
+    var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  }
+
+  var kittySchema = mongoose.Schema({
+    name: String
+  });
+
+  var Kitten = mongoose.model('Kitten', kittySchema);
+
+  var silence = new Kitten({ name: 'Silence' });
+  console.log(silence.name); // 'Silence'
+
+  var fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.speak(); // "Meow name is fluffy"
+});
