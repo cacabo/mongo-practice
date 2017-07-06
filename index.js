97a9d423f5cf3cd53133d7225bfc575bf8de1349
Cameron Cabo <ccabo@seas.upenn.edu>
2017-07-06 13:40:36 -0700

    got basic db running on localhost

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/mongoBasics');
 
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('connecction successful');
-
-  kittySchema.methods.speak = () => {
-    var greeting = this.name
-      ? "Meow name is " + this.name
-      : "I don't have a name";
-    console.log(greeting);
-  }
-
-  var kittySchema = mongoose.Schema({
-    name: String
-  });
-
-  var Kitten = mongoose.model('Kitten', kittySchema);
-
-  var silence = new Kitten({ name: 'Silence' });
-  console.log(silence.name); // 'Silence'
-
-  var fluffy = new Kitten({ name: 'fluffy' });
-  fluffy.speak(); // "Meow name is fluffy"
+  console.log('connection successful');
 });
+
+// kittySchema.methods.speak = () => {
+//   var greeting = this.name
+//     ? "Meow name is " + this.name
+//     : "I don't have a name";
+//   console.log(greeting);
+// }
+//
+// var kittySchema = mongoose.Schema({
+//   name: String
+// });
+//
+// var Kitten = mongoose.model('Kitten', kittySchema);
+//
+// var silence = new Kitten({ name: 'Silence' });
+// console.log(silence.name); // 'Silence'
